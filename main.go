f3f1c1390ee3d40a1400ba80d45c4386ec349b8c
andrei <andreimcristof@gmail.com>
2018-11-19 17:17:15 +0100

    almost done

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 )
 
 func main() {
-	parse("https://www.google.se")
-	// print(r)
+	tagsWithContent := parse("https://www.google.se")
+
+	for i := 0; i < len(tagsWithContent); i++ {
+		println("content", tagsWithContent[i].content)
+		print("tag", tagsWithContent[i].tag)
+	}
+
 }
 
-func parse(url string) string {
+func parse(url string) []TagWithContent {
 	response := fetch(url)
 	parsed := ParseHTML(response)
-	return parsed.Data
+	return parsed
 }
 
 func fetch(url string) string {
--- parser.go
+++ parser.go
@@ -7,24 +7,33 @@ import (
 	"golang.org/x/net/html"
 )
 
+// TagWithContent a tag and its value content
+type TagWithContent struct {
+	tag     string
+	content string
+}
+
 // ParseHTML parse an html content recursively and return result as a struct
-func ParseHTML(content string) *html.Node {
+//TODO: export as pkg somehow
+func ParseHTML(content string) []TagWithContent {
 	reader := strings.NewReader(content)
 	htmldoc, err := html.Parse(reader)
 	if err != nil {
 		os.Exit(2)
 	}
-	print(htmldoc.FirstChild.Data)
+
 	var recursiveIteratorFunc func(*html.Node)
 	recursiveIteratorFunc = func(thisNode *html.Node) {
+		if thisNode.Type == html.ElementNode {
+			t := thisNode.FirstChild
+			if t != nil {
+				println(t.Data)
+			}
+		}
 		for child := thisNode.FirstChild; child != nil; child = child.NextSibling {
-			// if thisNode.Type == html.ElementNode {
-			// 	println(thisNode.Data)
-			// }
-			println(thisNode.Data)
 			recursiveIteratorFunc(child)
 		}
 	}
 
-	return htmldoc
+	recursiveIteratorFunc(htmldoc)
 }
